cf5e9ccf31b2f8c5f91b970b30b43247204bf6ca
Christian Schulze <christian.schulze@mni.thm.de>
2016-11-30 08:15:46 +0100

    added datasets module with first dataset: yeast

---
+++ skml/datasets/__init__.py
@@ -0,0 +1,1 @@
+from .load_datasets import load_dataset
---
+++ skml/datasets/load_datasets.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+from sklearn.datasets import fetch_mldata
+
+
+def load_dataset(name):
+    """
+    Loads a multi-label classification dataset.
+
+    Parameter
+    ---------
+    name : "yeast"
+        Name of the dataset.
+    """
+    if name == 'yeast':
+        data = fetch_mldata('yeast')
+        X = data.data
+        y = data.target.toarray().astype(np.int).T
+
+        return (X, y)
+    else:
+        raise Exception("No such dataset")
