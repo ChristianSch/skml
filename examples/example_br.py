f15215fe201479df694b7ea808930c7db7bb341a
Christian Schulze <accounts@andinfinity.de>
2018-02-27 17:07:03 +0100

    [wip] improved examples; see #12
    Signed-off-by: Christian Schulze <accounts@andinfinity.de>

--- examples/example_br.py
+++ examples/example_br.py
@@ -12,38 +12,41 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 import numpy as np
 
 from skml.problem_transformation import BinaryRelevance
 from skml.datasets import load_dataset
 
 X, y = load_dataset('yeast')
-clf = BinaryRelevance(RandomForestClassifier())
-clf.fit(X, np.array(y))
-y_pred = clf.predict(X)
-y_pred_proba = clf.predict_proba(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y)
+
+clf = BinaryRelevance(LogisticRegression())
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
 
 print("hamming loss: ")
-print(hamming_loss(y, y_pred))
+print(hamming_loss(y_test, y_pred))
 
 print("accuracy:")
-print(accuracy_score(y, y_pred))
+print(accuracy_score(y_test, y_pred))
 
 print("f1 score:")
 print("micro")
-print(f1_score(y, y_pred, average='micro'))
+print(f1_score(y_test, y_pred, average='micro'))
 print("macro")
-print(f1_score(y, y_pred, average='macro'))
+print(f1_score(y_test, y_pred, average='macro'))
 
 print("precision:")
 print("micro")
-print(precision_score(y, y_pred, average='micro'))
+print(precision_score(y_test, y_pred, average='micro'))
 print("macro")
-print(precision_score(y, y_pred, average='macro'))
+print(precision_score(y_test, y_pred, average='macro'))
 
 print("recall:")
 print("micro")
-print(recall_score(y, y_pred, average='micro'))
+print(recall_score(y_test, y_pred, average='micro'))
 print("macro")
-print(recall_score(y, y_pred, average='macro'))
+print(recall_score(y_test, y_pred, average='macro'))
