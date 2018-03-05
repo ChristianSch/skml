043f1368122ce8906c0a1bb3e1e39bb9a8ce69c9
Christian Schulze <accounts@andinfinity.de>
2018-03-05 10:36:45 +0100

    [wip] preliminary sparse support

--- skml/datasets/sample_down_label_space.py
+++ skml/datasets/sample_down_label_space.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.sparse import issparse
 from operator import itemgetter
 
 
@@ -25,15 +26,23 @@ def sample_down_label_space(y, k, method='most-frequent'):
 
     if method == 'most-frequent':
         # create mapping of frequencies per column (label)
-        freqs = list(map(lambda x: (x[0], x[1]), enumerate(np.sum(y, axis=0))))
+        if issparse(y):
+            # sum of sparse matrix returns a matrix. A1 holds the matrix as
+            # a one dimensional array, just like if y was dense
+            freqs = list(map(lambda x: (x[0], x[1]),
+                             enumerate(np.sum(y, axis=0).A1)))
+        else:
+            freqs = list(map(lambda x: (x[0], x[1]),
+                             enumerate(np.sum(y, axis=0))))
+
         freqs.sort(key=itemgetter(1), reverse=True)
+
         # select top k labels, restore original order
         # if we wouldn't restore the original order, the labels would
         # be ordered not by original column, but by "most frequent occuring"
-        # and could render classifications probably unusable
         sampled_indices = sorted(list(map(lambda x: x[0], freqs[:k])))
 
-        return np.take(y, sampled_indices, axis=1)
+        return y[:, sampled_indices]
 
     else:
         raise ValueError('No such sample method {0}'.format(method))
