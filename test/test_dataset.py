5a26635aec1f0ed3c95babf96955d412b63d0f15
Christian Schulze <accounts@andinfinity.de>
2018-03-05 10:37:29 +0100

    added test for sparse sample down label space

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -1,4 +1,5 @@
 from chai import Chai
+from scipy import sparse
 
 from skml.datasets import load_dataset, sample_down_label_space
 
@@ -17,6 +18,12 @@ class TestDataset(Chai):
         assert sample5.shape[1] == 5
 
         self.assert_raises(ValueError, sample_down_label_space, y, 20)
+
+    def test_sparse_sample_down_label_space(self):
+        y = sparse.rand((200, 20))
+        sample10 = sample_down_label_space(y, 10)
+
+        assert sample10.shape[1] == 10
 
     def test_load_enron(self):
         X, y = load_dataset('enron', 'undivided')
